5c0e14afb334603f51e81b845edd1f59929d0f58
sinon1509 <nghiaphan1509@gmail.com>
2023-01-16 22:09:40 +0900

    add texture animation

--- app.js
+++ app.js
@@ -24,3 +24,30 @@ AFRAME.registerComponent('registerevents', {
         });
     }
 });
+
+AFRAME.registerComponent('random-animation', {
+    schema: {
+        size: {type: 'number', default: 1.0}
+    },
+
+    init: function () {
+        var element = this.el;
+        element.setAttribute('scale', {
+            x: this.data.size,
+            y: this.data.size,
+            z: this.data.size
+        });
+        var rdX = Math.random() * 5;
+        var rdY = Math.random() * 5 + 1;
+        var rdZ = Math.random() * 5 + 1.5;
+        var dur = Math.random() * (5000-2300) + 2300;
+        element.setAttribute('animation', {
+            property: 'position',
+            from: '0 1 0',
+            to: rdX + ' ' + rdY + ' ' + rdZ,
+            dur: dur,
+            easing: 'linear',
+            loop: true
+        });
+    }
+})
