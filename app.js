7fe62f690950925aaaa360c0e633a07316e7d419
sinon1509 <nghiaphan1509@gmail.com>
2023-01-16 19:06:37 +0900

    add alert

---
+++ app.js
@@ -0,0 +1,18 @@
+window.addEventListener('camera-init', (data) => {
+    var root = document.getElementById("root")
+    root.style.visibility = 'hidden'
+})
+
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        var marker = this.el;
+
+        marker.addEventListener('markerFound', function() {
+            var root = document.getElementById("root")
+            root.style.visibility = 'visible'
+
+            var alert = document.getElementById("alert")
+            alert.style.visibility = 'hidden'
+        });
+    }
+});
