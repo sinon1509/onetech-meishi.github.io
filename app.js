1d25d909247c1f34b9eeab6afe369b04da0f157a
sinon1509 <nghiaphan1509@gmail.com>
2023-02-18 23:04:32 +0900

    add shadow add anchor

--- app.js
+++ app.js
@@ -3,31 +3,57 @@ window.addEventListener('camera-init', (data) => {
     root.style.visibility = 'hidden'
 })
 
-AFRAME.registerComponent('registerevents', {
+AFRAME.registerComponent('marker-anchor', {
     init: function () {
         var marker = this.el;
-
-        marker.addEventListener('markerFound', function() {
+        var isPlaying = false;
+        marker.addEventListener('markerFound', function () {
             var root = document.getElementById("root")
             root.style.visibility = 'visible'
 
             var alert = document.getElementById("alert")
             alert.style.visibility = 'hidden'
 
-            var bgm = document.querySelector('[sound]')
-            bgm.components.sound.playSound()
-        });
+            if (isPlaying === false) {
+                var bgm = document.querySelector('a-sound')
+                bgm.components.sound.playSound()
+            }
 
-        marker.addEventListener('markerLost', function() {
-            var bgm = document.querySelector('[sound]')
-            bgm.components.sound.stopSound()
+            var character = document.getElementById("character")
+            var plane = document.getElementById("plane")
+            var marker = document.querySelector('a-marker')
+            var position = marker.getAttribute('position')
+            var rotation = marker.getAttribute('rotation')
+            character.setAttribute('position', position)
+            character.setAttribute('rotation', rotation)
+
+            var planeRotation = rotation;
+            planeRotation.x = rotation.x - 90;
+            plane.setAttribute('position', position)
+            plane.setAttribute('rotation', rotation)
+            isPlaying = true;
         });
+    },
+    tick: function () {
+        var character = document.getElementById("character")
+        var plane = document.getElementById("plane")
+        var marker = document.querySelector('a-marker')
+        var position = marker.getAttribute('position')
+        var rotation = marker.getAttribute('rotation')
+        character.setAttribute('position', position)
+        character.setAttribute('rotation', rotation)
+
+        var planeRotation = rotation;
+        planeRotation.x = rotation.x - 90;
+        plane.setAttribute('position', position)
+        plane.setAttribute('rotation', rotation)
+
     }
 });
 
 AFRAME.registerComponent('random-animation', {
     schema: {
-        size: {type: 'number', default: 1.0}
+        size: { type: 'number', default: 1.0 }
     },
 
     init: function () {
@@ -40,7 +66,7 @@ AFRAME.registerComponent('random-animation', {
         var rdX = Math.random() * 5;
         var rdY = Math.random() * 5 + 1;
         var rdZ = Math.random() * 5 + 1.5;
-        var dur = Math.random() * (5000-2300) + 2300;
+        var dur = Math.random() * (5000 - 2300) + 2300;
         element.setAttribute('animation', {
             property: 'position',
             from: '0 1 0',
